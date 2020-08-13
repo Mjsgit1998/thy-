92b048d9fe25b253517679176c727532dc23e15d
gpf131012 <1654648712@qq.com>
2020-08-13 11:44:36 +0800

    登录—校验完成

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -7,20 +7,20 @@
               <img src="../../assets/img/logo_index.png" alt="">
           </div>
           <!-- 表单 -->
-          <el-form style="margin-top:20px">
+          <el-form :model="loginForm" :rules="loginRules" style="margin-top:20px">
               <!-- 一个表单域就是一个form-item-->
-              <el-form-item>
+              <el-form-item prop="mobile">
                   <!-- 手机号 -->
-                <el-input></el-input>
+                <el-input v-model="loginForm.mobile"></el-input>
               </el-form-item>
-              <el-form-item>
+              <el-form-item prop="code">
                   <!-- 验证码 -->
-                  <el-input style="width:200px"></el-input>
+                  <el-input style="width:200px" v-model="loginForm.code"></el-input>
                   <el-button style="float:right">发送验证码</el-button>
               </el-form-item>
-              <el-form-item>
+              <el-form-item prop="check">
                   <!-- 选框 -->
-                   <el-checkbox>我已阅读并同意你列举出来的<el-link type="primary">霸王条款</el-link></el-checkbox>
+                   <el-checkbox v-model="loginForm.check">我已阅读并同意你列举出来的<el-link type="primary">霸王条款</el-link></el-checkbox>
               </el-form-item>
               <el-form-item>
                    <el-button style="width:100%" type="primary">登录</el-button>
@@ -32,7 +32,42 @@
 
 <script>
 export default {
+  data () {
+    return {
+      // 需要校验的表单数据
+      loginForm: {
+        mobile: '', // 手机号
+        code: '', // 验证码
+        check: false // 是否勾选
+      },
+      //   校验规则
+      loginRules: {
+        //   key 校验的字段名： value （数组=》校验规则）
+        mobile: [
+          { required: true, message: '请输入手机号' },
+          { pattern: /^1[3456789]\d{9}$/, message: '请输入正确的手机号' }
+        ],
+        code: [
+          { required: true, message: '请输入您的验证码' },
+          {
+            pattern: /^\d{6}$/, message: '请输入6位正确验证码'
+          }
+        ],
+        check: [{
+          validator: function (rule, value, callback) {
+            // rule 当前的规则，value=>checked的值,callback => 回调函数
+            if (value) {
+              // 选中 通过
+              callback()// 直接执行表示直接通过
+            } else {
+              callback(new Error('您必须同意'))
+            }
+          }
+        }]
+      }
 
+    }
+  }
 }
 </script>
 
