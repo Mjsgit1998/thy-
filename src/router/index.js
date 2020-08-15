e1f9d01f6b15e2bce47e08557833fc7bedaaf2dd
gpf131012 <1654648712@qq.com>
2020-08-15 11:55:32 +0800

    路由导航守卫

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Layout from '../views/Layout'
 import Login from '../views/login'
+
+import left from '../views/left'
 Vue.use(VueRouter)
 
 const routes = [
@@ -19,6 +21,9 @@ const routes = [
   {
     path: '/login',
     component: Login
+  }, {
+    path: '/left',
+    component: left
   }
   // {
   //   path: '/about',
@@ -32,6 +37,29 @@ const routes = [
 
 const router = new VueRouter({
   routes
+})
+// 路由拦截器 该方法接收函数作为参数
+// 参数1： to 表示去哪里的路由信息
+// 参数2：  from 表示来自哪里的路由信息
+// 参数3： next 是一个方法，用于路由放行
+// 我们要做的就是 判断一下你用户是否处在登录状态，有通过，没有跳到登录
+router.beforeEach((to, from, next) => {
+  // console.log(to)
+  // console.log('所有的页面都要经过')
+  // 如果是登录页 直接放行
+  if (to.path === '/login') {
+    next()
+    return
+  }
+  // 非登录页
+  // 判断是否有 token
+  const token = window.localStorage.getItem('user-token')
+  if (token) {
+    next()
+  } else {
+    // 没有就跳回登录页
+    next('/login')
+  }
 })
 
 export default router
---
+++ src/views/left/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>声命周期
+      {{name}}
+    <input v-model="name">
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      name: '张三'
+    }
+  },
+  //   创建前后
+  beforCreate () {
+    //   不做处理
+    console.log('实例之前创建')
+  },
+  created () {
+    //   一般用作数据查询，没有完成页面渲染
+    console.log('实例创建之后')
+  },
+  //   页面渲染前后
+  beforeMount () {
+    //   也可以加载数据，依旧没有渲染页面
+    console.log('页面渲染前')
+  },
+  mounted () {
+    //   页面显然完成，可以获取DOM对象
+    console.log('页面渲染之后执行')
+  },
+  //   数据更新前后
+  beforUpdate () {
+    console.log('数据更新前执行')
+  },
+  updated () {
+    console.log('数据更新后执行')
+  },
+  //   实例销毁阶段
+  beforeDestroy () {
+    //   定时器，将之前定义的定时器销毁掉
+    console.log('销毁之前')
+  },
+  destroyed () {
+    console.log('销毁之后')
+  }
+}
+</script>
+
+<style>
+
+</style>
