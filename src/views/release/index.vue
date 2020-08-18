82f80a8d1aa5393247208ca4ab33afdca3dcbbdc
gpf131012 <1654648712@qq.com>
2020-08-18 14:49:09 +0800

    富文本编辑器

--- src/views/release/index.vue
+++ src/views/release/index.vue
@@ -1,13 +1,109 @@
 <template>
-  <div>发布文章</div>
+  <div class="publish">
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <span>发布文章</span>
+      </div>
+      <el-form ref="form" :model="article" label-width="80px">
+        <el-form-item label="标题">
+          <el-input v-model="article.title"></el-input>
+        </el-form-item>
+        <!-- 富文本 -->
+          <quill-editor
+            ref="myQuillEditor"
+            v-model="article.content"
+          />
+        <el-form-item label="内容">
+          <el-input type="textarea" v-model="article.content"></el-input>
+        </el-form-item>
+        <el-form-item label="频道">
+          <el-select v-model="article.channel_id" placeholder="请选择频道">
+            <el-option :label="channel.name" :value="channel.id"
+            v-for="channel in channels" :key="channel.id"
+            >
+            </el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="封面">
+          <el-radio-group v-model="article.cover">
+            <!-- <el-radio label=></el-radio>
+            <el-radio label="线下场地免费"></el-radio> -->
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onPublish(false)">发表</el-button>
+          <el-button @click="onPublish(true)">存入草稿</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
 </template>
 
 <script>
-export default {
+import Vue from 'vue'
+import VueQuillEditor, { quillEditor } from 'vue-quill-editor'
+import 'quill/dist/quill.core.css' // import styles
+import 'quill/dist/quill.snow.css' // for snow theme
+import 'quill/dist/quill.bubble.css' // for bubble theme
 
+Vue.use(VueQuillEditor /* { default global options } */)
+
+export default {
+  name: 'PublishArticle',
+  components: {
+    quillEditor
+  },
+  data () {
+    return {
+      article: {
+        title: '', // 文章标题
+        content: '', // 文章内容
+        cover: {
+          type: 0, // 封面类型 -1:自动，0-无图，1-1张，3-3张
+          images: [] // 图片，无图就是空数组即可
+        },
+        channel_id: ''
+      },
+      channels: []
+    }
+  },
+  created () {
+    this.loadChannels()
+  },
+  methods: {
+
+    loadChannels () {
+      this.$axios({
+        method: 'GET',
+        url: '/channels'
+      }).then(res => {
+        // console.log(res)
+        this.channels = res.data.data.channels
+      }).catch(err => {
+        console.log(err, '获取数据失败')
+      })
+    },
+    onPublish (draft) {
+      this.$axios({
+        method: 'POST',
+        url: '/articles',
+        headers: {
+          Authorization: `Bearer ${window.localStorage.getItem('user-token')}`
+        },
+        // Query 参数
+        params: {
+          draft
+        },
+        data: this.article
+      }).then(res => {
+        console.log(res)
+      }).catch(err => {
+        console.log(err, '发布失败')
+      })
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
 </style>
